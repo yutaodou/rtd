f2f8124a1e250994f460759418b23edf48ddbaca
Yutao Dou <yutao.dou@rea-group.com>
2020-04-14 16:25:32 +0800

    revise

--- src/command/done.rs
+++ src/command/done.rs
@@ -20,15 +20,21 @@ impl<'a> Done<'a> {
 impl<'a> Command for Done<'a> {
     fn run(self: &Self) -> Result<(), &'static str> {
         self.args.values_of("INPUT").unwrap().for_each(|id| {
-            let mut task_id = id;
-            let mut done = true;
-            if task_id.starts_with("~") {
-                task_id = &task_id[1..];
-                done = false;
-            };
-            let result = storage::done(task_id.parse().unwrap(), done).unwrap();
+            let (task_id, done) = parse(id).unwrap();
+            let result = storage::done(task_id, done).unwrap();
             single::render(&result, &mut stdout()).unwrap();
         });
         Ok(())
     }
+}
+
+fn parse(value: &str) -> Result<(u32, bool), &'static str> {
+    let mut task_id = value;
+    let mut done = true;
+    if value.starts_with("~") {
+        task_id = &value[1..];
+        done = false;
+    };
+    let id = task_id.parse().unwrap();
+    Ok((id, done))
 }

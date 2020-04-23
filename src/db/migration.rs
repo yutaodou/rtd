1ede3e826e2262fa35e8a6e8e754485058946fda
Dou Yutao <yutaodou@gmail.com>
2020-04-23 11:00:40 +0800

    add migration for due_date

--- src/db/migration.rs
+++ src/db/migration.rs
@@ -1,15 +1,16 @@
-use crate::db::storage;
-use anyhow::{Error, Result};
-use rusqlite::params;
-use rusqlite::Connection;
+use anyhow::Error as AnyError;
+use anyhow::Result as AnyResult;
+use migrant_lib::Direction::Up;
+use migrant_lib::{Config, EmbeddedMigration, Migrator, Settings};
+use std::error::Error;
 use std::fs::DirBuilder;
 
-pub fn migrate() -> Result<()> {
-    let conn = storage::open_connection();
-    create_database_dir().and_then(|_| init_database(&conn))
+pub fn migrate() -> AnyResult<()> {
+    create_database_dir()
+        .and_then(|_| apply_migrations().map_err(|error| AnyError::msg(error.to_string())))
 }
 
-fn create_database_dir() -> Result<()> {
+fn create_database_dir() -> AnyResult<()> {
     super::database_file_path()
         .parent()
         .map(|database_file_dir| {
@@ -18,22 +19,46 @@ fn create_database_dir() -> Result<()> {
             }
             ()
         })
-        .ok_or_else(|| Error::msg("Failed to create database dir."))
+        .ok_or_else(|| AnyError::msg("Failed to create database dir."))
 }
 
-fn init_database(conn: &Connection) -> Result<()> {
-    conn.execute(
-        "CREATE TABLE IF NOT EXISTS todo  (
-            id              INTEGER PRIMARY KEY,
-            title           TEXT NOT NULL,
-            done            INTEGER DEFAULT 0,
-            today           TEXT DEFAULT '',
-            list            TEXT DEFAULT '',
-            priority        TEXT DEFAULT 'medium',
-            created_at      INTEGER DEFAULT 0,
-            completed_at    INTEGER DEFAULT 0
-            )",
-        params![],
-    )?;
+fn apply_migrations() -> Result<(), Box<dyn Error>> {
+    let settings = Settings::configure_sqlite()
+        .database_path(super::database_file_path())?
+        .build()?;
+
+    let mut config = Config::with_settings(&settings);
+    config.setup()?;
+
+    config.use_migrations(&[
+        EmbeddedMigration::with_tag("add-todo-table")
+            .up(String::from(
+                "CREATE TABLE IF NOT EXISTS todo  (
+                    id              INTEGER PRIMARY KEY,
+                    title           TEXT NOT NULL,
+                    done            INTEGER DEFAULT 0,
+                    today           TEXT DEFAULT '',
+                    list            TEXT DEFAULT '',
+                    priority        TEXT DEFAULT 'medium',
+                    created_at      INTEGER DEFAULT 0,
+                    completed_at    INTEGER DEFAULT 0
+                )",
+            ))
+            .boxed(),
+        EmbeddedMigration::with_tag("add-due-date")
+            .up(String::from(
+                "ALTER TABLE todo ADD COLUMN due_date TEXT DEFAULT '';",
+            ))
+            .boxed(),
+    ])?;
+
+    let config = config.reload()?;
+
+    Migrator::with_config(&config)
+        .all(true)
+        .direction(Up)
+        .show_output(false)
+        .swallow_completion(true)
+        .apply()?;
     Ok(())
 }
